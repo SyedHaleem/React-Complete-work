6acf2c11fecd9f5f8bec04e23238bc4a19bd99a7
SyedHaleem <syedhaleem899@gmail.com>
2024-06-27 00:04:36 +0500

    ..

--- Ecommerce-Website/Frontend/src/components/Navbar.jsx
+++ Ecommerce-Website/Frontend/src/components/Navbar.jsx
@@ -38,6 +38,36 @@ function Navbar() {
       <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
         <div className="navbar">
           <div className="navbar-start">
+          <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><Link to='/'>Home</Link></li>
+        <li>
+          <a>Catagories</a>
+          <ul className="p-2">
+            <li><Link to='/mens'>Men's Fashion</Link></li>
+                    <li><Link to='/womens'>Women's Fashion</Link></li>
+                    <li><Link to='/kids'>Kid's Fashion</Link></li>
+          </ul>
+        </li>
+        <li> <Link to='/cart'>Cart</Link></li>
+      </ul>
+    </div>
             <Link to='/' className="text-2xl font-bold cursor-pointer">ClothStore</Link>
           </div>
           <div className="navbar-center hidden lg:flex">
